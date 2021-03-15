2db9547ef9380acfcee939ff0ed14341f74b8367
Olia <olga.shostak1991@gmail.com>
2021-03-15 23:53:48 +0200

    Updates

---
+++ precache-manifest.41882c7c3137cb9826df6d05e130b87c.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0439b14bb48f64753f2604d4b8c72af4",
+    "url": "/wallet_react/index.html"
+  },
+  {
+    "revision": "079483d317d74e8be7ba",
+    "url": "/wallet_react/static/css/10.febc6561.chunk.css"
+  },
+  {
+    "revision": "abc988f07bb199a5fee9",
+    "url": "/wallet_react/static/css/HomePage.3073f7f9.chunk.css"
+  },
+  {
+    "revision": "613239142e355f9bc85e",
+    "url": "/wallet_react/static/css/Login.1d9bc5fa.chunk.css"
+  },
+  {
+    "revision": "054db1458d672af69fce",
+    "url": "/wallet_react/static/css/Navigation.e8b6b49b.chunk.css"
+  },
+  {
+    "revision": "878157d46b2017dafa24",
+    "url": "/wallet_react/static/css/Register.0968f1b4.chunk.css"
+  },
+  {
+    "revision": "534ae83fe29a9fc23143",
+    "url": "/wallet_react/static/css/Statistic.bd1eda3b.chunk.css"
+  },
+  {
+    "revision": "2f37b4c0f2e811d33dc8",
+    "url": "/wallet_react/static/css/main.d3f6dc4c.chunk.css"
+  },
+  {
+    "revision": "e5435ff09fc400fc46cf",
+    "url": "/wallet_react/static/js/0.ac254dc7.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/wallet_react/static/js/0.ac254dc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079483d317d74e8be7ba",
+    "url": "/wallet_react/static/js/10.aab1784f.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/wallet_react/static/js/10.aab1784f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe89f8628a5d3db7004",
+    "url": "/wallet_react/static/js/8.447eff61.chunk.js"
+  },
+  {
+    "revision": "9014f75bee2b03e13e1876e3edc1005c",
+    "url": "/wallet_react/static/js/8.447eff61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0e7f81f9072bedf4c7",
+    "url": "/wallet_react/static/js/9.9ff8975e.chunk.js"
+  },
+  {
+    "revision": "c4381524cae000488e2fb889012466d0",
+    "url": "/wallet_react/static/js/9.9ff8975e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc988f07bb199a5fee9",
+    "url": "/wallet_react/static/js/HomePage.c7fa55ed.chunk.js"
+  },
+  {
+    "revision": "613239142e355f9bc85e",
+    "url": "/wallet_react/static/js/Login.37019436.chunk.js"
+  },
+  {
+    "revision": "054db1458d672af69fce",
+    "url": "/wallet_react/static/js/Navigation.3b2505d8.chunk.js"
+  },
+  {
+    "revision": "878157d46b2017dafa24",
+    "url": "/wallet_react/static/js/Register.fde9a35e.chunk.js"
+  },
+  {
+    "revision": "534ae83fe29a9fc23143",
+    "url": "/wallet_react/static/js/Statistic.a312e26d.chunk.js"
+  },
+  {
+    "revision": "2f37b4c0f2e811d33dc8",
+    "url": "/wallet_react/static/js/main.325a895a.chunk.js"
+  },
+  {
+    "revision": "7cc9423ccc433941f9bf",
+    "url": "/wallet_react/static/js/runtime-main.7fab54ae.js"
+  },
+  {
+    "revision": "762a5e79e8ffb3f463d8d4c0e7c21f61",
+    "url": "/wallet_react/static/media/+.762a5e79.svg"
+  },
+  {
+    "revision": "0bba4e4f2fc57927a8850282d69a9dda",
+    "url": "/wallet_react/static/media/Group.0bba4e4f.svg"
+  },
+  {
+    "revision": "c9875d64ce83c155e544f39881840948",
+    "url": "/wallet_react/static/media/HomeForMob.c9875d64.svg"
+  },
+  {
+    "revision": "6ae9f3bdabc4f6af282a3bb0f864c536",
+    "url": "/wallet_react/static/media/NavDiagram.6ae9f3bd.svg"
+  },
+  {
+    "revision": "bafc893908b5f9d16d82eee3a662b606",
+    "url": "/wallet_react/static/media/NavDiagramMob.bafc8939.svg"
+  },
+  {
+    "revision": "89497e5bf2ac94163b44a6436b40ec7a",
+    "url": "/wallet_react/static/media/PCRegIcon.89497e5b.svg"
+  },
+  {
+    "revision": "13fac8f1ec4675be7817237ff0f20987",
+    "url": "/wallet_react/static/media/TabletRegIcon.13fac8f1.svg"
+  },
+  {
+    "revision": "a286efcc0ad622aca66022ab2ab71588",
+    "url": "/wallet_react/static/media/Vector 4.a286efcc.svg"
+  },
+  {
+    "revision": "9d3c937c0ed79eba15d30ec0a3f88b00",
+    "url": "/wallet_react/static/media/calendar.9d3c937c.svg"
+  },
+  {
+    "revision": "2e9bc2e27ef93cec26556b30b09ad61b",
+    "url": "/wallet_react/static/media/delete.2e9bc2e2.svg"
+  },
+  {
+    "revision": "fd4d5d35cbd8ec93e66089ad3f8b97ce",
+    "url": "/wallet_react/static/media/down-arrow.fd4d5d35.svg"
+  },
+  {
+    "revision": "3adfdedbae6514cb5664cf32bf956c3b",
+    "url": "/wallet_react/static/media/edit.3adfdedb.svg"
+  },
+  {
+    "revision": "fdfe2e3ca03b8b680f2def1805afdfdd",
+    "url": "/wallet_react/static/media/error.fdfe2e3c.svg"
+  },
+  {
+    "revision": "fad6a1527aa9535df9309ce1e1c3d69d",
+    "url": "/wallet_react/static/media/exit 1.fad6a152.svg"
+  },
+  {
+    "revision": "321a6855e5cd91ed768d4773d59f5205",
+    "url": "/wallet_react/static/media/graphCurrency.321a6855.svg"
+  },
+  {
+    "revision": "e70fa46cd9a3c1ddc0235016992a5bb5",
+    "url": "/wallet_react/static/media/home.e70fa46c.svg"
+  },
+  {
+    "revision": "6b6ea8ac6b32bfa48afdf9450fcd0ece",
+    "url": "/wallet_react/static/media/img.6b6ea8ac.svg"
+  },
+  {
+    "revision": "c565ce038e6578490cb4f5b3a3a1719b",
+    "url": "/wallet_react/static/media/imgScreen.c565ce03.svg"
+  },
+  {
+    "revision": "194324087a9f95cb0235facbd2a13210",
+    "url": "/wallet_react/static/media/info.19432408.svg"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/wallet_react/static/media/montserrat-v15-latin-600.6fb1b562.woff2"
+  },
+  {
+    "revision": "7c839d15a6f54e7025ba8c0c4b333e8f",
+    "url": "/wallet_react/static/media/montserrat-v15-latin-600.7c839d15.woff"
+  },
+  {
+    "revision": "86f6cbd269e0c5a26f038a4d5ffc9cf1",
+    "url": "/wallet_react/static/media/montserrat-v15-latin-600.86f6cbd2.eot"
+  },
+  {
+    "revision": "a6924a5e072a58911c8588859f6825d5",
+    "url": "/wallet_react/static/media/montserrat-v15-latin-600.a6924a5e.svg"
+  },
+  {
+    "revision": "acb878a397bf674d7baa32a3267e5a3c",
+    "url": "/wallet_react/static/media/montserrat-v15-latin-600.acb878a3.ttf"
+  },
+  {
+    "revision": "5cc74ef8a4c422084726eb9dd1163b82",
+    "url": "/wallet_react/static/media/montserrat-v15-latin-regular.5cc74ef8.eot"
+  },
+  {
+    "revision": "6a9e85ac9247f5848db957b873c62e0c",
+    "url": "/wallet_react/static/media/montserrat-v15-latin-regular.6a9e85ac.ttf"
+  },
+  {
+    "revision": "8102c4838f9e3d08dad644290a9cb701",
+    "url": "/wallet_react/static/media/montserrat-v15-latin-regular.8102c483.woff"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/wallet_react/static/media/montserrat-v15-latin-regular.bc3aa95d.woff2"
+  },
+  {
+    "revision": "f3fef7e587e6c436df1d7985b2a90ada",
+    "url": "/wallet_react/static/media/montserrat-v15-latin-regular.f3fef7e5.svg"
+  },
+  {
+    "revision": "b6967563e20be06d9ce0b8319a96fcab",
+    "url": "/wallet_react/static/media/pinkCircle.b6967563.svg"
+  },
+  {
+    "revision": "d9b47b572d99507bcca83e20d2a20c6a",
+    "url": "/wallet_react/static/media/pinkCircleScreen.d9b47b57.svg"
+  },
+  {
+    "revision": "05c0ebe6c48bf8062f16cb0bb6b00218",
+    "url": "/wallet_react/static/media/poppins-v15-latin-700.05c0ebe6.woff"
+  },
+  {
+    "revision": "05d04c19767aac11a4b8b507ae8eb144",
+    "url": "/wallet_react/static/media/poppins-v15-latin-700.05d04c19.ttf"
+  },
+  {
+    "revision": "a0f595e06933e3e903b693c729c199db",
+    "url": "/wallet_react/static/media/poppins-v15-latin-700.a0f595e0.eot"
+  },
+  {
+    "revision": "b36aa291a196158c551f6fc353f39634",
+    "url": "/wallet_react/static/media/poppins-v15-latin-700.b36aa291.svg"
+  },
+  {
+    "revision": "f4f17fd53c7d040e56f91a3ecb692b22",
+    "url": "/wallet_react/static/media/poppins-v15-latin-700.f4f17fd5.woff2"
+  },
+  {
+    "revision": "342c989fe844c73c74298974f114b821",
+    "url": "/wallet_react/static/media/poppins-v15-latin-regular.342c989f.ttf"
+  },
+  {
+    "revision": "4fc29212bd42883c45edd0bfbd91ad72",
+    "url": "/wallet_react/static/media/poppins-v15-latin-regular.4fc29212.woff"
+  },
+  {
+    "revision": "78430a281058e0066b0429fec1e16cc4",
+    "url": "/wallet_react/static/media/poppins-v15-latin-regular.78430a28.eot"
+  },
+  {
+    "revision": "9ed361bba8488aeb2797b82befda20f1",
+    "url": "/wallet_react/static/media/poppins-v15-latin-regular.9ed361bb.woff2"
+  },
+  {
+    "revision": "c26cc61af85aabbf51bd458e1e4e8aa5",
+    "url": "/wallet_react/static/media/poppins-v15-latin-regular.c26cc61a.svg"
+  },
+  {
+    "revision": "a6185a076190fea900a160d38aa6f0a0",
+    "url": "/wallet_react/static/media/success.a6185a07.svg"
+  },
+  {
+    "revision": "ed63a0b941366c34de1d746bfe7f9f64",
+    "url": "/wallet_react/static/media/usd.ed63a0b9.svg"
+  },
+  {
+    "revision": "2ea7f9c799f0f54ae2ec538d73882fde",
+    "url": "/wallet_react/static/media/violetCircle.2ea7f9c7.svg"
+  },
+  {
+    "revision": "0bba4e4f2fc57927a8850282d69a9dda",
+    "url": "/wallet_react/static/media/wallet.0bba4e4f.svg"
+  },
+  {
+    "revision": "fdd888b687d56012a8ec91e40c94aa90",
+    "url": "/wallet_react/static/media/warning.fdd888b6.svg"
+  }
+]);
